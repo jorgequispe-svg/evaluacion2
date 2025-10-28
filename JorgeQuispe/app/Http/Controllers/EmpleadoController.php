3370b53a10243277c46fb68083334028480aa777
unknown <jorge.quispe@uab.edu.bo>
2025-10-27 23:40:32 -0400

    feat(api): crear controlador y rutas GET/POST empleados

---
+++ JorgeQuispe/app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empleado;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return Empleado::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'nombre'=>'required',
+            'apellido'=>'required',
+            'correo'=>'required|email|unique:empleados',
+            'salario'=>'required|numeric'
+        ]);
+        return Empleado::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
